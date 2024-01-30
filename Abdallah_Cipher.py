7623baa0b695ccb65beaed0d6a3e9173c68c6db0
AbdallahIshaq <157402282+AbdallahIshaq@users.noreply.github.com>
2024-01-30 22:04:47 +0200

    Create Abdallah_Cipher.py

---
+++ Abdallah_Cipher.py
@@ -0,0 +1,41 @@
+key=int(input("enter the Key:"))
+plaintext=input("Enter plaintext:").lower()
+alpha="abcdefghijklmnopqrstuvwxyz"
+op=key*2-2
+def encryption(key,plaintext,op):
+    tranpostext=""
+    global ciphertex
+    ciphertex=""
+    for row in range(key):
+        index=0
+        if row==0:
+            while index<len(plaintext):
+                tranpostext+=plaintext[index]
+                index+=op
+        elif row==key-1:
+            index=row
+            while index <len(plaintext):
+                tranpostext+=plaintext[index]
+                index+=op
+    for x in tranpostext:
+        ciphertex+=alpha[( alpha.index(x)+key)%26]
+    return ciphertex
+def Dencryption(key,Ciphertex,op):
+    tranpostext=""
+    text=""
+    for x in Ciphertex:
+        text +=alpha[( alpha.index(x)-key)%26]
+    for row in range(key):
+        if row==key-1:
+            index=row
+            while index <len(text):
+                tranpostext+=plaintext[index]
+                index+=op
+        elif row==0:
+            index=0
+            while index<len(text):
+                tranpostext+=plaintext[index]
+                index+=op
+    return tranpostext
+print(encryption(key,plaintext,op))
+print(Dencryption(key,ciphertex,op))
