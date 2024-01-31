b8d8e1efa55320c27c8291157e7c1f799e2415a1
AbdallahIshaq <157402282+AbdallahIshaq@users.noreply.github.com>
2024-01-31 04:25:06 +0200

    Update Abdallah_Cipher.py

--- Abdallah_Cipher.py
+++ Abdallah_Cipher.py
@@ -17,6 +17,15 @@ def encryption(key,plaintext,op):
             while index <len(plaintext):
                 tranpostext+=plaintext[index]
                 index+=op
+        else:
+            l_index=row
+            r_index=op-row
+            while l_index<len(plaintext):
+                tr+=plaintext[l_index]
+                if r_index<len(plaintext):
+                    tranpostext+=plaintext[r_index]
+                l_index+=op
+                r_index+=op
     for x in tranpostext:
         ciphertex+=alpha[( alpha.index(x)+key)%26]
     return ciphertex
