74bbf971c06391b939e521638d18f151b1e6db32
AbdallahIshaq <157402282+AbdallahIshaq@users.noreply.github.com>
2024-02-16 18:46:05 +0200

    Create rsa.py

---
+++ rsa.py
@@ -0,0 +1,15 @@
+from Crypto.Util.number import *    
+
+p = getPrime(1024)
+q = getPrime(1024)
+n = p * q              
+phi = (p-1) * (q-1)    
+e = (2**16)+1          
+d = pow(e, -1, phi)
+
+PT = bytes_to_long(b'rsa is cool')
+CT = pow(PT, e, n)
+decrypted = long_to_bytes(pow(CT, d, n))
+
+# demo
+print(f"plaintext: {PT}\nciphertext: {CT}\ndecrypted: {decrypted}")
